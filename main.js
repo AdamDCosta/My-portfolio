79104ddb53afdcecb24c7acddfe69c3fcb987e38
AdamDCosta <adam.dcosta@googlemail.com>
2021-09-07 15:26:49 +0100

    hamburger menu working

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+// const menuButton = document.getElementsByClassName('nav__menu')[0];
+// const navLinks = document.getElementsByClassName('nav__links')[0];
+
+// menuButton.addEventListener('click', () =>{
+//   navLinks.classList.toggle('active')
+// } );
+
 const menuButton = document.getElementsByClassName('nav__menu')[0];
-const navLinks = document.getElementsByClassName('nav__links')[1];
+const navLinks = document.getElementsByClassName('nav__links')[0];
 
 menuButton.addEventListener('click', () =>{
+  console.log(navLinks)
   navLinks.classList.toggle('active')
 } );
