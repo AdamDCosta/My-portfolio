cd13bb2ecf94c95f6b664a46b6f618e8e6470d00
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-24 15:18:18 +0100

    home links updated

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-// const menuButton = document.getElementsByClassName('nav__menu')[0];
-// const navLinks = document.getElementsByClassName('nav__links')[0];
+const menuButton = document.getElementsByClassName("nav__menu")[0];
+const navList = document.getElementsByClassName("nav__links")[0];
+const navLinks = document.querySelectorAll(".nav__links--li");
 
-// menuButton.addEventListener('click', () =>{
-//   navLinks.classList.toggle('active')
-// } );
 
-const menuButton = document.getElementsByClassName('nav__menu')[0];
-const navLinks = document.getElementsByClassName('nav__links')[0];
+menuButton.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
 
-menuButton.addEventListener('click', () =>{
-  console.log(navLinks)
-  navLinks.classList.toggle('active')
-} );
+navLinksArr = Array.from.navLinks
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navList.classList.remove("active");
+    navList.classList.add("nav__links");
+
+  })
+  
+});
+
+
